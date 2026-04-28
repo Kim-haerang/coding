2bb24bfa1e6cb8071d5f810008751de57c80c30e
Kim-haerang <mers0ei1@gmail.com>
2026-04-28 10:54:15 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int dp[1000][3];
+	int min_val=100001;
+	int index;
+	cin >> dp[0][0] >> dp[0][1] >> dp[0][2];
+
+	for (int i = 1; i < n; i++) {
+		cin >> dp[i][0] >> dp[i][1] >> dp[i][2];
+		dp[i][0] = dp[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
+		dp[i][1] = dp[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
+		dp[i][2] = dp[i][2] + min(dp[i - 1][1], dp[i - 1][0]);
+	}
+	int ans = min({ dp[n - 1][0], dp[n - 1][1], dp[n - 1][2] });
+	cout << ans;
+	return 0;
+}
