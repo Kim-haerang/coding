83a609b80a5f7387e089b2c6f43f4d0bc7b0a520
Kim-haerang <mers0ei1@gmail.com>
2026-04-28 10:21:44 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1536 ms, Memory: 2060 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int num[10001] = { 0 };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int temp = 0;
+		cin >> temp;
+		num[temp]++;
+	}
+	for (int i = 1; i < 10001; i++) {
+		for (int j = 0; j < num[i]; j++) {
+			cout << i << '\n';
+		}
+	}
+	return 0;
+}
