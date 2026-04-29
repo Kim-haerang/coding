3337eab150930f1e8037191e90a8a430f6243ee7
Kim-haerang <mers0ei1@gmail.com>
2026-04-29 09:03:25 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    if(!(a%2)&&!(b%2)) answer = abs(a-b);
+    else if (!(a%2)||!(b%2)) answer = 2*(a+b);
+    else answer = a*a+b*b;
+    return answer;
+}
