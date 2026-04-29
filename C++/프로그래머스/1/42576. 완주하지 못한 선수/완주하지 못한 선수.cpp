e609a84a37da5651c62c746118809772f2734aa2
Kim-haerang <mers0ei1@gmail.com>
2026-04-29 10:05:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.16 ms, Memory: 25.4 MB -BaekjoonHub

--- C++/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ C++/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,14 +1,15 @@
 #include <string>
+#include <algorithm>
 #include <vector>
-#include <unordered_map>
-
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    unordered_map<string, int> m;
-    for (auto& p : participant) m[p]++;
-    for (auto& c : completion) m[c]--;
-    for (auto& [name, cnt] : m) if (cnt) return name;
+	string answer = "";
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
 
-    return "";
+	for (int i = 0; i < completion.size(); i++) {
+		if (participant[i] != completion[i]) return participant[i];
+	}
+	return participant[participant.size() - 1];
 }
