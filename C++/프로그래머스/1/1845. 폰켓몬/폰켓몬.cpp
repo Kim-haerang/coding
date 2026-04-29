b18f735a7c9e16e1eed37eebe04816e6b4549cb8
Kim-haerang <mers0ei1@gmail.com>
2026-04-29 13:25:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.71 ms, Memory: 4.15 MB -BaekjoonHub

--- C++/프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ C++/프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,10 +1,9 @@
 #include <vector>
-#include <set>
+#include <unordered_set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    set<int> s;
-    for(auto& n : nums) s.insert(n);
+    unordered_set<int> s(nums.begin(), nums.end());
     return min(s.size(), nums.size() / 2);
 }
