1aee9d7290c74e01e308edc0919767d47a1261fa
Kim-haerang <mers0ei1@gmail.com>
2026-04-29 10:06:50 +0900

    Update 완주하지 못한 선수.cpp

--- C++/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ C++/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
 	string answer = "";
-	sort(participant.begin(), participant.end());
+	sort(participant.begin(), participant.end()); 
 	sort(completion.begin(), completion.end());
 
 	for (int i = 0; i < completion.size(); i++) {
-		if (participant[i] != completion[i]) return participant[i];
+		if (participant[i] != completion[i]) return participant[i]; // 미완주자가 한명이라서 사용할 수 있는 방법
 	}
 	return participant[participant.size() - 1];
 }
