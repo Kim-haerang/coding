f3e6fd6f9fbe646a408da13cf3676945b57a5c6b
Kim-haerang <mers0ei1@gmail.com>
2026-05-01 16:09:44 +0900

    [level 1] Title: 유연근무제, Time: 0.17 ms, Memory: 4.22 MB -BaekjoonHub

--- C++/프로그래머스/1/388351. 유연근무제/유연근무제.cpp
+++ C++/프로그래머스/1/388351. 유연근무제/유연근무제.cpp
@@ -1,11 +1,8 @@
-#include <string>
 #include <vector>
-
 using namespace std;
 
-int toMinute(int t) {
-    int h = t / 100;
-    int m = t % 100;
+int toMin(int t) {
+    int h = t / 100, m = t % 100;
     return h * 60 + m;
 }
 
@@ -15,21 +12,19 @@ int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) 
     for (int i = 0; i < schedules.size(); i++) {
         bool pass = true;
 
-        int limit = toMinute(schedules[i]) + 10;
+        int limit = toMin(schedules[i]) + 10;
 
         for (int j = 0; j < 7; j++) {
             int day = (startday - 1 + j) % 7 + 1;
 
             if (day == 6 || day == 7) continue;
 
-            if (toMinute(timelogs[i][j]) > limit) {
+            if (toMin(timelogs[i][j]) > limit) {
                 pass = false;
                 break;
             }
         }
-
         if (pass) answer++;
     }
-
     return answer;
 }
