e678cc7195d8592822d512faefbdf2e9372d4f26
Kim-haerang <mers0ei1@gmail.com>
2026-05-13 14:21:09 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 3.94 MB -BaekjoonHub

--- C++/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ C++/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -7,14 +7,13 @@ vector<int> solution(vector<string> wallpaper) {
     int H = wallpaper.size();
     int W = wallpaper[0].size();
 
-    int mx = 1000000, my = 1000000;
-    int Mx = -1000000, My = -1000000;
+    int mx = W, my = H;
+    int Mx = -1, My = -1;
     for (int y = 0; y < H; y++) {
         for (int x = 0; x < W; x++) {
-            if (wallpaper[y][x] == '#') {
-                mx = min(mx, x); my = min(my, y);
-                Mx = max(Mx, x); My = max(My, y);
-            }
+            if (wallpaper[y][x] != '#') continue;
+            mx = min(mx, x); my = min(my, y);
+            Mx = max(Mx, x); My = max(My, y);
         }
     }
     return { my, mx, My+1, Mx+1 };
